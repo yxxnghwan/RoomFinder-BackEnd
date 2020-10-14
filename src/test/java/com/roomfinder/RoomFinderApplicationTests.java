ccccf051e38fa092e261d2e41e9f5407a1ecea81
yxxnghwan <younghwan960@gmail.com>
2020-10-15 01:02:46 +0900

    roomfinder first

---
+++ src/test/java/com/roomfinder/RoomFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
